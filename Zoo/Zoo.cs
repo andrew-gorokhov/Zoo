1b2754f8480290f9ce98f0f67d0673ebaeef1900
andrew-gorokhov <147326548+andrew-gorokhov@users.noreply.github.com>
2023-11-05 22:42:29 +0300

    Add files via upload

---
+++ Zoo/Zoo.cs
@@ -0,0 +1,75 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        public string Name {  get; set; }
+       
+        public string Sound {  get; set; }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine($"{Name} издает звук {Sound}");
+        }
+    }
+    public class Tiger : Animal
+    {
+        public Tiger(string name, string sound) : base() 
+        {
+            Name = name;
+            Sound = sound;
+        }
+    }
+
+    public class Giraffe : Animal
+    {
+        public Giraffe(string name, string sound) : base()
+        {
+            Name = name;
+            Sound = sound;
+        }
+    }
+
+    public class Bear : Animal
+    {
+        public Bear(string name, string sound) : base()
+        {
+            Name = name;
+            Sound = sound;
+        }
+    }
+
+    public class Penguin : Animal
+    {
+        public Penguin(string name, string sound) : base()
+        {
+            Name = name;
+            Sound = sound;
+        }
+    }
+
+    public class Moose : Animal
+    {
+        public Moose(string name, string sound) : base()
+        {
+            Name = name;
+            Sound = sound;
+        }
+    }
+
+    public class Rabbit : Animal
+    {
+        public Rabbit(string name, string sound) : base()
+        {
+            Name = name;
+            Sound = sound;
+        }
+    }
+
+    public class Hippopotamus : Animal
+    {
+        public Hippopotamus(string name, string sound) : base()
+        {
+            Name = name;
+            Sound = sound;
+        }
+    }
+}
---
+++ Зоопарк/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Зоопарк
+{
+using Zoo;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] Zoo = new Animal[6];
+
+            Zoo[0] = new Tiger("Ричард", "рычит");
+            Zoo[1] = new Giraffe("Жора", "жует листву");
+            Zoo[2] = new Bear("Медвежонок", "рычит");
+            Zoo[3] = new Penguin("Пингвин", "шумит");
+            Zoo[4] = new Moose("Лось", "торчит");
+            Zoo[5] = new Rabbit("Заяц", "пищит");
+
+            foreach (Animal animal in Zoo)
+            {
+                if (animal != null)
+                {
+                    animal.MakeSound();
+                }
+            }
+        }
+    }
+}
